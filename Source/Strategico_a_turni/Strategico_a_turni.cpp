f5a5d2129b372569eed77308c06e9f0b7c2e4f56
Giulia Righi <righi.giulia@outlook.com>
2025-03-01 21:52:58 +0100

    Initial commit

---
+++ Source/Strategico_a_turni/Strategico_a_turni.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Strategico_a_turni.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Strategico_a_turni, "Strategico_a_turni" );
---
+++ Source/Strategico_a_turniEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Strategico_a_turniEditorTarget : TargetRules
+{
+	public Strategico_a_turniEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("Strategico_a_turni");
+	}
+}
